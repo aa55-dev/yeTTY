d2c8554bbf5528a50c1fccaf6a17da775db27c7d
AA55 <mail@aa55.dev>
2025-05-25 12:37:55 +0530

    Fix dbus case

--- dbus_common.hpp
+++ dbus_common.hpp
@@ -1,10 +1,10 @@
 #ifndef DBUS_COMMON_HPP
 #define DBUS_COMMON_HPP
 
-inline static constexpr const char* DBUS_SERVICE_NAME = "dev.aa55.yeTTY";
-inline static constexpr const char* DBUS_SERVICE_REGEX = R"(^dev\.aa55\.yeTTY.id-[0-9]+$)";
+inline static constexpr const char* DBUS_SERVICE_NAME = "dev.aa55.yetty";
+inline static constexpr const char* DBUS_SERVICE_REGEX = R"(^dev\.aa55\.yetty.id-[0-9]+$)";
 
-inline static constexpr const char* DBUS_INTERFACE_NAME = "dev.aa55.yeTTY.control";
+inline static constexpr const char* DBUS_INTERFACE_NAME = "dev.aa55.yetty.control";
 
 inline static constexpr const char* DBUS_START = "start";
 inline static constexpr const char* DBUS_STOP = "stop";
